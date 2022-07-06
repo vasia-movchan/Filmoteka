3220b72384d88f80e93a1311e589680cb3170075
Serj <mrnobody959@gmail.com>
2022-07-06 19:00:39 +0300

    added movie rating to each card

--- src/js/render-elements.js
+++ src/js/render-elements.js
@@ -17,13 +17,13 @@ export function renderMovieCard({ poster_path, id, title, genres, release_date, 
         <img class="movie-card__image" src="${BASE_URL}${poster_path}" loading="lazy" alt="movie poster">
         <p class="movie-card__title">${title}</p>
         <div class="movie-card__wrapper">
-          <p class="movie-card__info">${genres} | ${release_date.slice(0, 4)}</p>  
+          <p class="movie-card__info">${genres} | ${release_date.slice(0, 4)}</p>
+          <div class="movie-card__rating">
+            <div class="movie-card__rating-icon" width="14" height="14"></div>
+            <p class="movie-card__rating-number">${vote_average.toFixed(1)}</p>
+          </div>
         </div>
       </a>
     </div>` 
 }
 
-{/* <div class="movie-card__rating">
-  <p class="movie-card__rating-text">Viewer's Rating</p>
-  <p class="movie-card__rating-number">${vote_average.toFixed(1)}</p>
-</div> */}
