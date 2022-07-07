b38c49d744ce6e662577ec98a724364cc15f66d5
AndreyHerzhov <andreyherzhov@gmail.com>
2022-07-07 15:29:54 +0300

    js/url

--- src/index.js
+++ src/index.js
@@ -2,21 +2,9 @@ import * as modal_team from './js/modal-team';
 import 'animate.css';
 import './js/header';
 import './js/load-homepage';
-<<<<<<< HEAD
-// import './js/pagination';
-import './js/url';
-=======
 import './js/modal-team'; 
->>>>>>> main
 import * as firebase from "./js/firebase-auth.js"
-
 import './js/top-button';
-/*
- * 1) Создать обьект FilmApiService
- * 2) Создать обьект PagesButton
- * 3) Написать функцию для рендера разметки фильмотеки по макету
- * 4) Написать функцию для переключения страниц
- *
- */
+ 
 
 
